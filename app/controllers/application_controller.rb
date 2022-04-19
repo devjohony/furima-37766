eff02b0bda5dc7e21c39975781896363f242e220
devjohony <101238941+devjohony@users.noreply.github.com>
2022-04-19 12:24:40 +0900

    カラムの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,10 @@ class ApplicationController < ActionController::Base
       username == 'admin' && password == '8888'
     end
   end
+  
+  protected
+
+  def configure_permittied_parameters
+    devicse_paramater_sanitizer.permit(:sign_up, key: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
+end
 end
