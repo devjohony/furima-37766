25aef54a4faa1e0525d28139a2c64dbe9c1bd4fd
devjohony <101238941+devjohony@users.noreply.github.com>
2022-04-22 12:23:45 +0900

    商品出品機能機能の1時コミット

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,57 @@
 class ItemsController < ApplicationController
+  before_action :require_login, only: :new, alert: 'You need to sign in or sign up before continuing.'
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
 
+  def index
+    # @items = Item.all.order('created_at DESC')
+  end
+
+  def new
+    @product = Item.new
+  end
+
+  def create
+    @product = Item.new(product_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+end
+
+def show
+end
+
+def edit
+end
+
+def update
+  if @item.update(item_params)
+    redirect_to item_path(@item.id)
+  else
+    render :edit
+  end
+end
+
+def destroy
+  if @item.destroy
+    redirect_to root_path
+  else
+    redirect :show
+  end
+end
+
+private
+
+def require_login
+  redirect_to user_session_path, alert: 'You need to sign in or sign up before continuing.' unless user_signed_in?
+end
+
+def item_params
+  params.require(:product).permit(:image, :name, :explanation, :category_id, :derively_fee_id, :status_id, :prefecture_id, :day_id, :price).merge(user_id: current_user.id)
+end
+
+def set_product
+  @product = Item.find(params[:id])
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'items#index'
+  resources :items do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
 end
