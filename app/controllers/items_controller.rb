19ad4a00b2321644f5bfcb54439ed249f2f1d0cf
devjohony <101238941+devjohony@users.noreply.github.com>
2022-04-24 21:27:00 +0900

    出品テストコード追記と修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :require_login, only: :new, alert: 'You need to sign in or sign up before continuing.'
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  # before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     # @items = Item.all.order('created_at DESC')
