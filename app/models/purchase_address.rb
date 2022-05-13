187b877cf6a65eac74b0168a7250611774ce0360
devjohony <101238941+devjohony@users.noreply.github.com>
2022-05-13 19:12:36 +0900

    レビュー修正２

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -12,7 +12,7 @@ class PurchaseAddress
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
     validates :user_id
     validates :item_id
-    validates :building_name
+    
   end
 
   def save
